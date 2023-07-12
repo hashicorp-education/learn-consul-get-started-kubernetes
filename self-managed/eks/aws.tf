ddb55a8815b52c39bd2cf3295782952641997f2e
Tu Nguyen <im2nguyen@gmail.com>
2023-07-12 07:43:12 -0700

    update aws ebs addon version (deprecated)

--- self-managed/eks/aws.tf
+++ self-managed/eks/aws.tf
@@ -127,7 +127,7 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.5.2-eksbuild.1"
+  addon_version            = "v1.20.0-eksbuild.1"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"
